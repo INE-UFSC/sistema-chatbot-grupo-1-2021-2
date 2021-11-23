30539bf59210464cc8b1e8ee6c68047de1e869ad
TorresLino <arthur.torres.de.lino@gmail.com>
2021-11-23 11:45:11 -0300

    setup botjose app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
+from Bots.BotJose import BotJose
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+lista_bots = [BotJose('José')]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
